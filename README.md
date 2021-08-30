c509433748980f9bbcc854c7e1eb7b4265ddfe0e
Adam Oresten <adamoresten@gmail.com>
2021-08-30 15:31:19 +0100

    Fixed merge

--- README.md
+++ README.md
@@ -1,39 +1,87 @@
-# Global Tags
+# Tagger
 
 This module allows you to tag objects and retrieve them just as easy.
 
-### Download
+## Download
 
 `https://github.com/Haxxer/FoundryVTT-GlobalTags/releases/latest/download/module.json`
 
-### Get By Tag
+## Dialogs
 
-Get all objects on the canvas with specific tags with:
-```js
-GlobalTags.getByTag(string|array)
+All major objects' configuration dialogues (such as tokens, tiles, walls, lights, etc), now has a "Tags" field. Each tag is separated by a comma.
+
+![img.png](docs/token-config.png)
+
+## Documentation
+
+### Get By Tag
+Gets objects with matching tags provided to the method.
+
+`Tagger.getByTag(string|array, options={})`
+
+```
+@param 	{string|array}	inTags	 An array of tags or a string of tags (separated by commas) that will be searched for
+@param 	{object} 	options	 An optional object that can contain any of the following:
+                                    - matchAll {boolean}         - whether the object must contain all of the provided tags
+                                    - caseInsensitive {boolean}	 - whether the search is case insensitive (capitals vs lowercase is not considered)
+                                    - objects {array} 	         - an array of objects to test
+                                    - ignore {array}	         - an array of objects to ignore
+                                    - sceneId {string}	         - a string ID for the scene to search in
+                                    
+@returns {Promise} 		A promise that will resolve when all objects have been found, returning an array of objects
+```
+
+Examples:
+
+- `Tagger.getByTag("tags")`
+- `Tagger.getByTag("tags, TO, LOOK, foR", { caseInsensitive: false })`
+- `Tagger.getByTag(["tags", "to", "look", "for"], { matchAll: true, sceneId: "8xjy4UUVoRcEYUNy" })`
+
+### Get Tags
+Gets all tags from a given object
+
+`Tagger.getTags(object)`
+
+```
+@param	{object} 	inObject	The object get tags from
+
+@returns {array} 			The tags on the objects in an array
 ```
 
 ### Add Tags
-Tag objects with:
-```js
-GlobalTags.addTags(object|array, string|array)
+
+Adds tags to an object
+
+`Tagger.addTags(object|array, string|array)`
+
+```
+@param 	{object|array}	inObjects	An object or a list of objects to add tags to
+@param 	{string|array} 	inTags		An array of tags or a string of tags (separated by commas) that will be added to the object
+
+@returns {promise} 			A promise that will resolve when the object's tags have been updated
 ```
 
 ### Remove Tags
-Remove tags from objects with:
-```js
-GlobalTags.removeTags(object|array, string|array)
+
+Removes tags from an object
+
+`Tagger.removeTags(object|array, string|array)`
+
+```
+@param 	{object|array}	inObjects	An object or a list of objects to remove tags from
+@param 	{string|array} 	inTags		An array of tags or a string of tags (separated by commas) that will be removed from the object
+
+@returns {promise} 			A promise that will resolve when the object's tags have been updated
 ```
 
 ### Clear All Tags
-Remove _all_ tags from objects with:
-```js
-GlobalTags.clearAllTags(object|array)
+
+Removes all tags from an object
+
+`Tagger.clearAllTags(object|array)`
+
 ```
+@param 	{object|array}	inObjects	The object to remove all tags from
 
-### Get Tags
-
-Get all tags from an object with:
-```js
-GlobalTags.getTags(object)
+@returns {promise}			A promise that will resolve when the object's tags have been updated
 ```
--- scripts/module.js
+++ scripts/module.js
@@ -1,135 +1,83 @@
-class GlobalTagsManager{
+const MODULE_NAME = "tagger";
+const FLAG_NAME = "tags";
+const Tagger = {
 
 	/**
-	 * Adds tags to an object
+	 * Gets all tags from a given object
 	 *
 	 * @param	{object} 			inObject	The object get tags from
 	 * @returns {array} 						The tags on the objects in an array
 	 */
 	getTags(inObject){
 		if(inObject?.document) inObject = inObject.document;
-		return inObject?.getFlag("global-tags", "tags") || [];
-	}
+		let tags = inObject?.getFlag(MODULE_NAME, FLAG_NAME) ?? [];
+		tags = Tagger._validateTags(tags);
+		return tags;
+	},
 
 	/**
-	 * Adds tags to an object
+	 * Gets objects with matching tags provided to the method
 	 *
-	 * @param 	{string|array}		inTags		An array of tags or a string of tags (separated by spaces) that will be searched for
-	 * @param 	{object} 			options		Options (TBD)
+	 * @param 	{string|array}		inTags		An array of tags or a string of tags (separated by commas) that will be searched for
+	 * @param 	{object} 			options		An optional object that can contain any of the following:
+	 * 												- matchAll {boolean} - whether the object must contain all of the provided tags
+	 * 												- caseInsensitive	 - whether the search is case insensitive (capitals vs lowercase is not considered)
+	 * 												- objects {array} 	 - an array of objects to test
+	 * 												- ignore {array}	 - an array of objects to ignore
+	 * 												- sceneId {string}	 - a string ID for the scene to search in
 	 * @returns {Promise} 						A promise that will resolve when all objects have been found, returning an array of objects
 	 */
 	getByTag(inTags, options = {}){
 
 		return new Promise(resolve => {
 
-			inTags = this._validateTags(inTags, "getByTag")
-				.join(" ")
-				.replace(/[^A-Za-z0-9 .*_-]/g, "")
-				.replace(".", "\.")
-				.replace("*", "(.*?)")
-				.split(" ")
-				.map(s => new RegExp(s));
+			options = foundry.utils.mergeObject({
+				objects: [],
+				ignore: [],
+				matchAll: false,
+				caseInsensitive: false,
+				sceneId: game.canvas.id
+			}, options)
 
-			let objs = new Set();
-			canvas.layers.forEach(l => {
-				l?.objects?.children.forEach(obj => {
-					let tags = this.getTags(obj);
-					if (tags && this._testTags(inTags, tags, options)) objs.add(obj);
-				});
-			})
+			inTags = Tagger._validateTags(inTags, "getByTag")
+				.map(t => options.caseInsensitive ? t.toLowerCase() : t)
+				.map(t => new RegExp(t.replace(/[^A-Za-z0-9 .*_-]/g, "").replace(".", "\.").replace("*", "(.*?)")))
 
-			resolve(Array.from(objs));
-		});
-	}
+			if(!Array.isArray(options.objects)) throw new Error("Tagger | getByTag | options.objects must be of type array");
+			if(!Array.isArray(options.ignore)) throw new Error("Tagger | getByTag | options.ignore must be of type array");
+			if(typeof options.sceneId !== "string") throw new Error("Tagger | getByTag | options.sceneId must be of type string");
+			let scene = game.scenes.get(options.sceneId);
+			if(!scene) throw new Error(`Tagger | getByTag | could not find scene with id ${options.sceneId}`);
 
-	/**
-	 * Adds tags to an object
-	 *
-	 * @param 	{object|array}	inObjects	An object or a list of objects to add tags to
-	 * @param 	{string|array} 	inTags		An array of tags or a string of tags (separated by spaces) that will be added to the object
-	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
-	 */
-	async addTags(inObjects, inTags){
-		inTags = this._validateTags(inTags, "addTags");
-		return this._updateTags(inObjects, inTags);
-	}
-
-	/**
-	 * Removes tags from an object
-	 *
-	 * @param 	{object|array}	inObjects	An object or a list of objects to remove tags from
-	 * @param 	{string|array} 	inTags		An array of tags or a string of tags (separated by spaces) that will be removed from the object
-	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
-	 */
-	async removeTags(inObjects, inTags){
-		inTags = this._validateTags(inTags, "removeTags");
-		return this._updateTags(inObjects, inTags, false);
-	}
-
-	/**
-	 * Removes all tags from an object
-	 *
-	 * @param 	{object|array}	inObjects	The object to remove all tags from
-	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
-	 */
-	async clearAllTags(inObjects){
-		return this._updateTags(inObjects, false);
-	}
-
-	/**
-	 * Updates tags on the object
-	 *
-	 * @param 	{object|array}	inObjects	The object to remove all tags from
-	 * @param 	{array|boolean}	inTags		The tags to update the object with, or false if clearing all tags
-	 * @param 	{boolean}		isAdding		The tags to update the object with, or false if clearing all tags
-	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
-	 */
-	_updateTags(inObjects, inTags, isAdding = true ){
-		inObjects = this._validateObjects(inObjects);
-		return new Promise(async (resolve) => {
-			for(let obj of inObjects){
-				if(inTags){
-					let tags = this.getTags(obj);
-					if(tags) tags = new Set(tags);
-					if(isAdding){
-						tags = new Set([...tags, ...inTags]);
-					}else{
-						inTags.forEach(t => tags.delete(t));
-					}
-					if(tags.size === 0){
-						await obj.unsetFlag("global-tags", "tags");
-						continue;
-					}
-					await obj.setFlag("global-tags", "tags", Array.from(tags));
-				}else{
-					await obj.unsetFlag("global-tags", "tags");
-				}
+			if(!options.objects.length){
+				options.objects = Tagger._getObjectsFromScene(scene)
 			}
-			resolve();
-		})
-	}
 
-	_validateTags(inTags, functionName){
-		if(!(typeof inTags === "string" || Array.isArray(inTags))) throw new Error(`GlobalTags | ${functionName} | inTags must be of type string or array`);
+			options.objects = options.objects.filter(o => !options.ignore.includes(o));
 
-		if(typeof inTags === "string"){
-			inTags = inTags.split(" ");
-		}
+			resolve(options.objects.filter(obj => {
+				let tags = Tagger.getTags(obj);
+				return tags && Tagger._testTags(inTags, tags, options);
+			}));
 
-		inTags.forEach(t => {
-			if(typeof t !== "string") throw new Error(`GlobalTags | ${functionName} | tags in array must be of type string`);
 		});
+	},
 
-		return inTags;
-	}
-
-	_validateObjects(inObjects){
-		if(!Array.isArray(inObjects)) inObjects = [inObjects];
-		inObjects = inObjects.map(o => o?.document ?? o);
-		return inObjects;
-	}
+	_getObjectsFromScene(scene){
+		return [
+			...Array.from(scene.tokens),
+			...Array.from(scene.lights),
+			...Array.from(scene.sounds),
+			...Array.from(scene.templates),
+			...Array.from(scene.tiles),
+			...Array.from(scene.walls),
+			...Array.from(scene.drawings),
+		].deepFlatten().filter(Boolean)
+	},
 
 	_testTags(inTestTags, inTags, options){
+
+		inTags = inTags.map(tag => options.caseInsensitive ? tag.toLowerCase() : tag)
 
 		let matches = inTestTags.filter(testTag => {
 			return inTags.filter(tag => {
@@ -142,14 +90,176 @@ class GlobalTagsManager{
 		}
 
 		return matches.length;
+	},
+
+	/**
+	 * Adds tags to an object
+	 *
+	 * @param 	{object|array}	inObjects	An object or a list of objects to add tags to
+	 * @param 	{string|array} 	inTags		An array of tags or a string of tags (separated by commas) that will be added to the object
+	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
+	 */
+	async addTags(inObjects, inTags){
+		inTags = Tagger._validateTags(inTags, "addTags");
+		inObjects = Tagger._validateObjects(inObjects);
+		return Tagger._updateTags(inObjects, inTags);
+	},
+
+	/**
+	 * Removes tags from an object
+	 *
+	 * @param 	{object|array}	inObjects	An object or a list of objects to remove tags from
+	 * @param 	{string|array} 	inTags		An array of tags or a string of tags (separated by commas) that will be removed from the object
+	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
+	 */
+	async removeTags(inObjects, inTags){
+		inTags = Tagger._validateTags(inTags, "removeTags");
+		inObjects = Tagger._validateObjects(inObjects);
+		return Tagger._updateTags(inObjects, inTags, false);
+	},
+
+	/**
+	 * Removes all tags from an object
+	 *
+	 * @param 	{object|array}	inObjects	The object to remove all tags from
+	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
+	 */
+	async clearAllTags(inObjects){
+		inObjects = Tagger._validateObjects(inObjects);
+		return Tagger._updateTags(inObjects, false);
+	},
+
+	/**
+	 * Updates tags on the object
+	 *
+	 * @param 	{object|array}	inObjects	The object to remove all tags from
+	 * @param 	{array|boolean}	inTags		The tags to update the object with, or false if clearing all tags
+	 * @param 	{boolean}		isAdding		The tags to update the object with, or false if clearing all tags
+	 * @returns {promise} 					A promise that will resolve when the object's tags have been updated
+	 */
+	_updateTags(inObjects, inTags, isAdding = true ){
+		return new Promise(async (resolve) => {
+			for(let obj of inObjects){
+				if(inTags){
+					let tags = Tagger.getTags(obj);
+					if(tags) tags = new Set(tags);
+
+					if(isAdding){
+						tags = new Set([...tags, ...inTags]);
+					}else{
+						inTags.forEach(t => tags.delete(t));
+					}
+					if(tags.size === 0){
+						await obj.unsetFlag(MODULE_NAME, FLAG_NAME);
+						continue;
+					}
+					await obj.setFlag(MODULE_NAME, FLAG_NAME, Array.from(tags));
+				}else{
+					await obj.unsetFlag(MODULE_NAME, FLAG_NAME);
+				}
+			}
+			resolve();
+		})
+	},
+
+	_validateTags(inTags, functionName){
+		if(!(typeof inTags === "string" || Array.isArray(inTags))) throw new Error(`Tagger | ${functionName} | inTags must be of type string or array`);
+
+		if(typeof inTags === "string") inTags = inTags.split(",");
+
+		inTags.forEach(t => {
+			if(typeof t !== "string") throw new Error(`Tagger | ${functionName} | tags in array must be of type string`);
+		});
+
+		inTags = inTags.map(t => t.trim());
+
+		return inTags;
+	},
+
+	_validateObjects(inObjects){
+		if(!Array.isArray(inObjects)) inObjects = [inObjects];
+		return inObjects.map(o => o?.document ?? o);
+	},
+
+	_applyHtml(app, elem, insertBefore = false){
+		if(!elem) return;
+		let obj = app?.object._object;
+		if(!obj) return;
+		let tags = Tagger.getTags(obj)
+		let html = `<fieldset style="margin: 3px 0;">
+			<legend>Tags</legend>
+			<div class="form-group">
+				<input name="flags.${MODULE_NAME}.${FLAG_NAME}" type="text" value="${tags.join(', ')}">
+			</div>
+		</fieldset>
+		`;
+		if(insertBefore){
+			$(html).insertBefore(elem);
+		}else{
+			elem.append(html);
+		}
+		app.setPosition({ height: "auto" });
+	},
+
+	_handleTokenConfig(app, html){
+		let elem = html.find(`div[data-tab="character"]`);
+		Tagger._applyHtml(app, elem);
+	},
+
+	_handleTileConfig(app, html){
+		let elem = html.find(`div[data-tab="basic"]`);
+		Tagger._applyHtml(app, elem);
+	},
+
+	_handleDrawingConfig(app, html){
+		let elem = html.find(`div[data-tab="position"]`);
+		Tagger._applyHtml(app, elem);
+	},
+
+	_handleGenericConfig(app, html){
+		let elem = html.find(`button[name="submit"]`);
+		Tagger._applyHtml(app, elem, true);
+	},
+
+	_fixUpTags(app){
+		let obj = app?.object._object;
+		if(!obj) return;
+		[obj] = Tagger._validateObjects(obj);
+		let tags = Tagger.getTags(obj);
+		if(!tags.length){
+			obj.unsetFlag(MODULE_NAME, FLAG_NAME);
+		}else{
+			obj.setFlag(MODULE_NAME, FLAG_NAME, tags);
+		}
 	}
 
 }
 
-
+const configHandler = {
+	"TokenConfig": Tagger._handleTokenConfig,
+	"TileConfig": Tagger._handleTileConfig,
+	"DrawingConfig": Tagger._handleDrawingConfig,
+	"WallConfig": Tagger._handleGenericConfig,
+	"LightConfig": Tagger._handleGenericConfig,
+	"AmbientSoundConfig": Tagger._handleGenericConfig,
+	"MeasuredTemplateConfig": Tagger._handleGenericConfig,
+	"NoteConfig": Tagger._handleGenericConfig
+}
 
 Hooks.once('ready', async function() {
-
-	window.GlobalTags = new GlobalTagsManager();
-
+	window.Tagger = Tagger;
 });
+
+Hooks.on("renderFormApplication", (app, html, options) => {
+	if(!app) return;
+	let type = app?.constructor?.name ?? false;
+	if(!type || !Object.keys(configHandler).includes(type)) return;
+	configHandler[type](app, html, true);
+})
+
+Hooks.on("closeFormApplication", (app, html, options) => {
+	if(!app) return;
+	let type = app?.constructor?.name ?? false;
+	if(!type || !Object.keys(configHandler).includes(type)) return;
+	Tagger._fixUpTags(app)
+})
